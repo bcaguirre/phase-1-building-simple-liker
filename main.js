c9a7fefe78b82c47dd60bd91291395178c3d9df1
bernadette <aguirre.bernadette@outlook.com>
2022-05-07 11:57:32 -0600

    Completed Assignment

--- main.js
+++ main.js
@@ -2,15 +2,41 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
+const emptyHearts = document.querySelectorAll(".like-glyph");
+const modal = document.querySelector("#modal");
+modal.classList.add("modal");
+const message = document.querySelector("#modal-message");
+
+  emptyHearts.forEach((emptyHeart) => {
+    emptyHeart.addEventListener("click", () => {
+    mimicServerCall()
+
+  .then(() => {
+    if (emptyHeart.textContent !== FULL_HEART) {
+    emptyHeart.classList.add("activated-heart");
+    emptyHeart.textContent = FULL_HEART;
+  } else if (emptyHeart.textContent === FULL_HEART) {
+    emptyHeart.textContent = EMPTY_HEART;
+    emptyHeart.classList.remove("activated-heart");
+    }
+  })
+   .catch((result) => {
+    modal.classList.remove("modal");
+    message.textContent = result;
+
+    setTimeout(hidden, 3000);
+   });
+  });
+});
+function hidden() {
+  return modal.classList.add("modal");
+}
 // Your JavaScript code goes here!
-
-
-
-
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
-
+  
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
